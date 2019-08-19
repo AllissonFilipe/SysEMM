abd6b906167eaf9b502aecb9005ff9824cfffbd7
AllissonFilipe <alissonfilipe@hotmail.com>
2019-08-19 15:43:56 -0300

    ajustes para o postgres 4

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -20,67 +20,69 @@ class AdminController extends Controller
         $total_users = User::all()->count();
         $total_turmas = Turma::all()->count();
         
-        // $alunos = Aluno::select(DB::raw("COUNT(*) as count , MONTHNAME(created_at) as month")) mysql
-        $alunos = Aluno::select(DB::raw("COUNT(*) as count , to_char(created_at, 'Month') as month"))
-        ->orderBy("created_at")
-        // ->groupBy(DB::raw("month(created_at)")) mysql
-        ->groupBy(DB::raw("to_char(created_at,'Month')"))
-        ->get()->toArray();
-        $chart_array = array();
-        foreach($alunos as $data){
-        $n_data = [];
-        array_push($n_data,$data['month'],$data['count']);
-        array_push($chart_array,$n_data);
-        }
-        $lava = new Lavacharts;
-        $popularity = $lava->DataTable();
-        $popularity->addStringColumn('Country')
-        ->addNumberColumn('Alunos')
-        ->addRows($chart_array);
-        $lava->LineChart('demochart', $popularity, [
-        'title' => 'Alunos cadastrados por mês',
-        'animation' => [
-        'startup' => true,
-        'easing' => 'inAndOut'
-        ],
-        'colors' => ['orange', '#ff8c1a']
-        ]);
+        // // $alunos = Aluno::select(DB::raw("COUNT(*) as count , MONTHNAME(created_at) as month")) mysql
+        // $alunos = Aluno::select(DB::raw("COUNT(*) as count , to_char(created_at, 'Month') as month"))
+        // ->orderBy("created_at")
+        // // ->groupBy(DB::raw("month(created_at)")) mysql
+        // ->groupBy(DB::raw("to_char(created_at,'Month')"))
+        // ->get()->toArray();
+        // $chart_array = array();
+        // foreach($alunos as $data){
+        // $n_data = [];
+        // array_push($n_data,$data['month'],$data['count']);
+        // array_push($chart_array,$n_data);
+        // }
+        // $lava = new Lavacharts;
+        // $popularity = $lava->DataTable();
+        // $popularity->addStringColumn('Country')
+        // ->addNumberColumn('Alunos')
+        // ->addRows($chart_array);
+        // $lava->LineChart('demochart', $popularity, [
+        // 'title' => 'Alunos cadastrados por mês',
+        // 'animation' => [
+        // 'startup' => true,
+        // 'easing' => 'inAndOut'
+        // ],
+        // 'colors' => ['orange', '#ff8c1a']
+        // ]);
 
-        $lava->DonutChart('donutchart', $popularity, [
-            'title' => 'Alunos cadastrados por mês'
-        ]);
+        // $lava->DonutChart('donutchart', $popularity, [
+        //     'title' => 'Alunos cadastrados por mês'
+        // ]);
 
-        //Usuários
-        // $users = User::select(DB::raw("COUNT(*) as count , MONTHNAME(created_at) as month")) mysql
-        $users = User::select(DB::raw("COUNT(*) as count , to_char(created_at, 'Month') as month"))
-        ->orderBy("created_at")
-        // ->groupBy(DB::raw("month(created_at)")) mysql
-        ->groupBy(DB::raw("to_char(created_at,'Month')"))
-        ->get()->toArray();
-        $chart_array_user = array();
-        foreach($users as $data){
-        $n_data = [];
-        array_push($n_data,$data['month'],$data['count']);
-        array_push($chart_array_user,$n_data);
-        }
-        $lava_user = new Lavacharts;
-        $popularity_user = $lava_user->DataTable();
-        $popularity_user->addStringColumn('Country')
-        ->addNumberColumn('Usuários')
-        ->addRows($chart_array_user);
-        $lava_user->LineChart('demochartUser', $popularity_user, [
-        'title' => 'Usuários cadastrados por mês',
-        'animation' => [
-        'startup' => true,
-        'easing' => 'inAndOut'
-        ],
-        'colors' => ['orange', '#ff8c1a']
-        ]);
+        // //Usuários
+        // // $users = User::select(DB::raw("COUNT(*) as count , MONTHNAME(created_at) as month")) mysql
+        // $users = User::select(DB::raw("COUNT(*) as count , to_char(created_at, 'Month') as month"))
+        // ->orderBy("created_at")
+        // // ->groupBy(DB::raw("month(created_at)")) mysql
+        // ->groupBy(DB::raw("to_char(created_at,'Month')"))
+        // ->get()->toArray();
+        // $chart_array_user = array();
+        // foreach($users as $data){
+        // $n_data = [];
+        // array_push($n_data,$data['month'],$data['count']);
+        // array_push($chart_array_user,$n_data);
+        // }
+        // $lava_user = new Lavacharts;
+        // $popularity_user = $lava_user->DataTable();
+        // $popularity_user->addStringColumn('Country')
+        // ->addNumberColumn('Usuários')
+        // ->addRows($chart_array_user);
+        // $lava_user->LineChart('demochartUser', $popularity_user, [
+        // 'title' => 'Usuários cadastrados por mês',
+        // 'animation' => [
+        // 'startup' => true,
+        // 'easing' => 'inAndOut'
+        // ],
+        // 'colors' => ['orange', '#ff8c1a']
+        // ]);
 
-        $lava_user->DonutChart('donutchartUser', $popularity_user, [
-            'title' => 'Usuários cadastrados por mês'
-        ]);
+        // $lava_user->DonutChart('donutchartUser', $popularity_user, [
+        //     'title' => 'Usuários cadastrados por mês'
+        // ]);
 
-        return view('admin.home.index', compact('total_alunos','total_professores','total_users','total_turmas','lava','lava_user'));
+        return view('admin.home.index', compact('total_alunos','total_professores','total_users','total_turmas'));
     }
 }
+
+// ,'lava','lava_user'
